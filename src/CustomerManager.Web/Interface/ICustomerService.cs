eb7ee352a5e359a29dc84a65ef1699551fd0dbd9
Israel Correa <raeldev@outlook.com>
2024-07-17 14:05:50 -0300

    Definindo interfaces para as depencias do Repository e Services

---
+++ src/CustomerManager.Web/Interface/ICustomerService.cs
@@ -0,0 +1,13 @@
+﻿using CustomerManager.Web.Models;
+
+namespace CustomerManager.Web.Interface
+{
+	public interface ICustomerService
+	{
+		Task<int> RegisterAsync(Customer customer);
+		Task<int> EditAsync(Customer customer, int customerId);
+		Task<int> RemoveAsync(object id);
+		Task<Customer> GetAsync(int id);
+		Task<IEnumerable<Customer>> GetAllAsync();
+	}
+}
